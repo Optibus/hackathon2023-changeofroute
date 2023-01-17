e86966c1bfe347c9533f823339caed68d31b30f4
Elad Alfassa <elad.alfassa@optibus.com>
2023-01-17 18:47:46 +0200

    Add cancelled stops
    The icon itslef is uploaded to a mapbox style

--- src/components/MapContainer.tsx
+++ src/components/MapContainer.tsx
@@ -2,6 +2,7 @@ import { useMemo } from "react";
 import Map, { Layer, Source } from "react-map-gl";
 import { Route, Stop } from "../types";
 import { FeatureCollection } from "geojson";
+import React from "react";
 
 const TOKEN =
   "pk.eyJ1IjoiZWxhZGFsZmFzc2EiLCJhIjoiY2xkMDRmaGY2MTM3aDNxcWx3dHYzZjl3eiJ9.AM2H8oWyhMuLgC6Rqnp84g";
@@ -36,6 +37,21 @@ const MapContainer = ({
     [stops]
   );
 
+  const cancelledStopsGeojson: FeatureCollection = useMemo(
+    () => ({
+      type: "FeatureCollection",
+      features:
+        cancelledStops?.map((stop) => ({
+          type: "Feature",
+          geometry: stop.geometry,
+          properties: {
+            name: stop.name,
+            color: stop.color,
+          },
+        })) ?? [],
+    }),
+    [cancelledStops]
+  );
   const routesGeojson: FeatureCollection = useMemo(
     () => ({
       type: "FeatureCollection",
@@ -59,19 +75,37 @@ const MapContainer = ({
         zoom: 14,
       }}
       style={{ width, height }}
-      mapStyle="mapbox://styles/mapbox/streets-v12"
+      mapStyle="mapbox://styles/eladalfassa/cld0gjrc0005614qtts9tdzhk"
       mapboxAccessToken={TOKEN}
     >
       <Source id="routes" type="geojson" data={routesGeojson}>
         <Layer
           type="line"
-          paint={{ "line-color": ["get", "color"], "line-width": 5 }}
+          paint={{ "line-color": ["get", "color"], "line-width": 12 }}
+        />
+      </Source>
+      <Source id="cancelledStops" type="geojson" data={cancelledStopsGeojson}>
+        <Layer
+          type="symbol"
+          layout={{
+            "icon-allow-overlap": true,
+            "icon-ignore-placement": true,
+            "text-allow-overlap": true,
+            "text-ignore-placement": true,
+            "icon-size": 1,
+            "icon-image": "CancelledStop",
+          }}
         />
       </Source>
       <Source id="stops" type="geojson" data={stopsGeojson}>
         <Layer
           type="circle"
-          paint={{ "circle-radius": 10, "circle-color": ["get", "color"] }}
+          paint={{
+            "circle-radius": 10,
+            "circle-color": ["get", "color"],
+            "circle-stroke-color": "white",
+            "circle-stroke-width": 3,
+          }}
         />
       </Source>
     </Map>
