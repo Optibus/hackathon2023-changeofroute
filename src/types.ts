c5b038313856ab53c4fabdeb69155cf88532b939
galgendler <109071218+galgendler@users.noreply.github.com>
2023-01-17 17:07:56 +0200

    added types

--- src/types.ts
+++ src/types.ts
@@ -4,7 +4,7 @@ export type Stops = FeatureCollection;
 export type Stop = {
   name: string;
   id: string;
-  geometry: Point;
+  geometry?: Point;
   isCancelled?: boolean;
 };
 export type Route = {
@@ -17,3 +17,28 @@ export type Segment = {
   geometry: LineString;
   color?: string;
 };
+
+export type WalkingDistance = {
+    minutes: number;
+    meters: number;
+  }
+export type StopAlternative = {
+    stop?: Stop;
+    walkingDistance?: WalkingDistance;
+    color?: string;
+}
+  
+
+export type PosterData = {
+    cancelledStop: Array<Stop>;
+    alternatives: Array<StopAlternative>
+    stop: Stop;
+    stops: Array<Stop>;
+    routes: Array<Route>;
+    cancelledSegments: Array<Route>;
+    dates: {
+        start?: Date,
+        end?: Date
+    }
+    logo: string; // URL to the image
+}
