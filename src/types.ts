db272e00e4e093274a0d8b79d73a6c688d0fe9e2
galgendler <109071218+galgendler@users.noreply.github.com>
2023-01-17 14:35:06 +0200

    add stop type

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,9 @@
-import { FeatureCollection, LineString } from "geojson";
+import { FeatureCollection, LineString, Point } from "geojson";
 
 export type Stops = FeatureCollection;
 export type Routes = LineString[];
+export type Stop = {
+    name: string;
+    id: string;
+    geometry: Point
+}
