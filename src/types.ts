37217dddfe0db3cee88b268ec44e63146270a919
galgendler <109071218+galgendler@users.noreply.github.com>
2023-01-17 14:48:41 +0200

    added types

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,18 @@
 import { FeatureCollection, LineString, Point } from "geojson";
 
 export type Stops = FeatureCollection;
-export type Routes = LineString[];
 export type Stop = {
     name: string;
     id: string;
     geometry: Point
+}
+export type Route = {
+    name: string;
+    color: string;
+    geometry: LineString;
+}
+
+export type Segment = {
+    geometry: LineString;
+    color?: string;
 }
