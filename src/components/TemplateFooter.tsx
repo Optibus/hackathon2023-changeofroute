a8752f3fae11ad225b6eff7a13c47b4b1620723c
Maoritzio <maor.haber@optibus.com>
2023-01-18 11:51:57 +0200

    go go

--- src/components/TemplateFooter.tsx
+++ src/components/TemplateFooter.tsx
@@ -1,11 +1,12 @@
 import styled from "styled-components";
+import logo from "../icons/logo.svg";
 
 const TemplateWrapper = styled("div")({
   position: "absolute",
   bottom: 0,
   left: 0,
   backgroundColor: "#001231",
-  padding: "3% 0",
+  padding: "2% 0",
   textAlign: "center",
   color: "#fff",
   width: "100%",
@@ -13,6 +14,17 @@ const TemplateWrapper = styled("div")({
 const TemplateFooter = () => {
   return (
     <TemplateWrapper>
+      <img
+        src={logo}
+        alt="Current stop"
+        width="10%"
+        style={{
+          left: 0,
+          position: "absolute",
+          top: 0,
+          transform: "translate(20%,-50%)",
+        }}
+      />
       09 366 6400 | Auckland Transport General Private Bag 92250, Auckland 1142
     </TemplateWrapper>
   );
